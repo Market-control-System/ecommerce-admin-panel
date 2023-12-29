f2f5fdb661db2e1a93a616339931753a4c578d54
Sheff2000 <sheffpb@gmail.com>
2023-12-29 16:49:43 +0200

    edit api route

--- front/src/api-service/auth-service.js
+++ front/src/api-service/auth-service.js
@@ -6,7 +6,7 @@
  */
 const sendLogin = async (phoneNumber, password) => {
     try {
-        const response = await fetch('http://localhost:3000/login', {
+        const response = await fetch('http://localhost:3000/api/auth/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
