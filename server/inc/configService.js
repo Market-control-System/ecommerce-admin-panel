4cd1d42369f1b6832de74f3d654dbe02722683b7
Sheff2000 <sheffpb@gmail.com>
2024-01-02 14:17:12 +0200

    update config

--- server/inc/configService.js
+++ server/inc/configService.js
@@ -1,10 +1,23 @@
-export const configDBPr = {
+import 'dotenv/config';
+export const configDBProfitools = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: process.env.DB_NAME_PROFITOOLS,
 };
-
+export const configDBMotoservice = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME_MOTOSERVICE,
+};
+// connect DB admin panel 
+export const configDBPanel = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME_PANEL,
+};
 export const configAuth = {
     secret_key: 'prKey',
     expires_key: '10h',
--- server/server.js
+++ server/server.js
@@ -15,5 +15,5 @@ app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}  IP - ${process.env.DB_HOST}`);
 });
