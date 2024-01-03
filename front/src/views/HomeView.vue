9228e6dd225686d97af82f8d68099265344968d4
Sheff2000 <sheffpb@gmail.com>
2024-01-03 12:05:58 +0200

    init home page template

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -6,10 +6,36 @@ const authStore = useAuthStore();
 const exitBtn = () => {
   authStore.logout();
 };
+
 </script>
 
 <template>
   <div class="container">
     <button class="btn btn-primary" @click="exitBtn">Exit</button>
+    <div class="row">
+      <div class="col-3">
+        <div class="side-bar-logo">Admin Panel</div>
+        {{ authStore.user }}
+      </div>
+      <div class="col-9">
+
+      </div>
+    </div>
+    
   </div>
 </template>
+
+<style>
+
+.container {
+  background-color: var(--bs-yellow-300);
+}
+.side-bar-logo {
+    height: var(--bs-app-header-height);
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    position: relative;
+    flex-shrink: 0;
+}
+</style>
