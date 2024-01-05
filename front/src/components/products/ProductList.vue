f1ec2639a5dcd653fec9383525cd4205192c883d
Sheff2000 <sheffpb@gmail.com>
2024-01-05 10:10:43 +0200

    init test component - prod list

---
+++ front/src/components/products/ProductList.vue
@@ -0,0 +1,17 @@
+<script setup>
+//import { defineProps } from 'vue';
+
+const props = defineProps({
+  productList: {
+    type: Array,
+    required: true,
+  }
+});
+</script>
+
+<template>
+    <div class="alert alert-info" v-for="product in props.productList" :key="product.id">
+        Name: {{ product.titleZapchast }} <br/>
+        Ostastok: {{ product.ostatok }} <br/>
+    </div>
+</template>
--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,6 +1,9 @@
 <script setup>
 //import '../assets/styles/mainCarcas.scss';
+import { ref } from 'vue';
 import { useAuthStore } from '@/store/auth';
+import {getProductList} from '@/api-service/product-service.js';
+import ProductList from '@/components/products/ProductList.vue';
 
 const authStore = useAuthStore();
 
@@ -8,11 +11,24 @@ const exitBtn = () => {
   authStore.logout();
 };
 
+let prList = ref([]);
+
+const loadProductClick = async () => {
+      const res = await getProductList('profitools', {filtr:'all'});
+      if (!res.err) {
+        prList.value = res.data;
+        console.log(prList.value);
+      } else {
+        console.log(res.err);
+      }
+}
+
 </script>
 
 <template>
   <div class="container ">
     <button class="btn btn-primary" @click="exitBtn">Exit</button>
+    <button class="btn btn-danger" @click="loadProductClick">Load product</button>
     <div class="row">
       <div class="col-3">
         <div class="left-menu">
@@ -22,6 +38,7 @@ const exitBtn = () => {
       </div>
       <div class="col-9">
         {{ authStore.user }}
+          <ProductList :productList = "prList"/>
       </div>
     </div>
     
