a38b8174b58685168221be346c848f883db781d8
Sheff2000 <sheffpb@gmail.com>
2024-01-05 09:58:26 +0200

    product router group

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express, { json } from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import authRoutes from './routes/authRoutes.js';
+import productRoutes from './routes/productRoutes.js';
 import 'dotenv/config';
 
 const app = express();
@@ -13,6 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Подключение маршрутов
 app.use('/api/auth', authRoutes);
+app.use('/api/product', productRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
