59dc670c3ce4d75e469d3c656ec55dda467c076a
Sheff2000 <sheffpb@gmail.com>
2024-02-02 23:10:00 +0200

    add throw

--- server/utils/dbQueries.js
+++ server/utils/dbQueries.js
@@ -1,28 +1,28 @@
 import { poolDBPanel, poolDBMotoservice, poolDBProfitools } from './database.js';
 
 const pools = {
-  panel: poolDBPanel,
-  profitools: poolDBProfitools,
-  motoservice: poolDBMotoservice
+    panel: poolDBPanel,
+    profitools: poolDBProfitools,
+    motoservice: poolDBMotoservice
 };
 
 export const executeQuery = async (dbName = 'panel', query, params) => {
-  const pool = pools[dbName];
-  if (!pool) {
-    throw new Error(`Database pool for '${dbName}' not found.`);
-  }
-
-  try {
-    const [rows] = await pool.query(query, params);
-    return rows;
-  } catch (error) {
-    console.error(`Error executing query on '${dbName}':`, error);
-    // Обработка ошибки подключения
-    if (error.code === 'ECONNREFUSED') {
-      // Код для обработки ошибки подключения:
-      console.error(`Database '${dbName}' is not accessible.`);
-      // добавить логику переподключения или возврата специфической ошибки
+    const pool = pools[dbName];
+    if (!pool) {
+        throw new Error(`Database pool for '${dbName}' not found.`);
     }
-    throw error;  
-  }
+
+    try {
+        const [rows] = await pool.query(query, params);
+        return rows;
+    } catch (error) {
+        console.error(`Error executing query on '${dbName}':`, error);
+        // Обработка ошибки подключения
+        if (error.code === 'ECONNREFUSED') {
+          // Код для обработки ошибки подключения:
+          console.error(`Database '${dbName}' is not accessible.`);
+          // добавить логику переподключения или возврата специфической ошибки
+        }
+        throw error;  
+    }
 };
--- server/utils/tokenGenerator.js
+++ server/utils/tokenGenerator.js
@@ -2,48 +2,47 @@ import jwt from 'jsonwebtoken';
 import { configAuth } from '../inc/configService.js';
 
 const generate = async (data) => {
-  try {
-    //data = { id, role, userName, dateCreate, dateUpdate, block }
-    const token = jwt.sign(
-      data, 
-      configAuth.secret_key,
-      { expiresIn: configAuth.expires_key }
-    );
-  
-    return {err:false, token:token};
-
-  } catch(err) {
-    return {err:true, msg:`Error in generate token - ${err}`}
-  }
+    try {
+        //data = { id, role, userName, dateCreate, dateUpdate, block }
+        const token = jwt.sign(
+            data, 
+            configAuth.secret_key,
+            { expiresIn: configAuth.expires_key }
+        );
+    
+        return token;
+    } catch(err) {
+        throw new Error(`Error generating token`);
+    }
 };
 
 const decodeToken = async (token) => {
-  try {
-    console.log('decoded token');
-    return jwt.verify(token, configAuth.secret_key);
-  } catch (err) {
-    throw new Error(`Error decode token - ${err}`);
-  }
+    try {
+        console.log('decoded token');
+        return jwt.verify(token, configAuth.secret_key);
+    } catch (err) {
+        throw new Error(`Error decode token - ${err}`);
+    }
 };
 
 const verifyToken = (req, res, next) => {
-  const token = req.headers['authorization']?.split(' ')[1];
-  if (!token) {
-    console.log('error token');
-    return res.send({err:true, msg:'A token is required for authentication'});
-  }
-
-  try {
-    console.log('ver token ', token);
-    req.user = jwt.verify(token, configAuth.secret_key);
-    console.log('user id - ', req.user);
-    next();
-  } catch (err) {
-    if (err.name === 'TokenExpiredError') {
-      return res.send({ err: true, msg: 'Token expired', tokenExpired: true });
+    const token = req.headers['authorization']?.split(' ')[1];
+    if (!token) {
+        console.log('error token');
+        throw new Error(`A token is required for authentication`);
     }
-    return res.send({err:true, msg:'Invalid Token'});
-  }
+
+    try {
+        console.log('ver token ', token);
+        req.user = jwt.verify(token, configAuth.secret_key);
+        console.log('user id - ', req.user);
+        next();
+    } catch (err) {
+        if (err.name === 'TokenExpiredError') {
+            throw new Error(`Token expired - the end. PLs, login now`);
+        }
+        throw new Error(`Invalid token`);
+    }
 };
 
 const tokenUtil = { generate, verifyToken };
